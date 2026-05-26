453b03d0e61091f8a0536cc5a6538004259d1643
Ylamicka <jezekvkybly@gmail.com>
2026-05-26 10:19:04 +0200

    commit #11

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Raceyear::index');
+$routes->get('zavody/rok/(:num)', 'Racedetail::index/$1');
--- app/Views/cyklo.php
+++ app/Views/cyklo.php
@@ -1,38 +1,16 @@
 <?=$this->extend("layout/master");?>
 
 <?=$this->section("content");?>
-    <h1>Ky$ :3</h1>
-<?php
-$table = new \CodeIgniter\View\Table();
 
-    $pole = array('Roky', 'Počet Závodů');
-$table->setHeading($pole);
-foreach($raceYear as $row){
-    $table->addRow($row->year,$row->pocet);
-}
-    $template = array(
-'table_open'=> '<table class="table table-bordered">',
-'thead_open'=> '<thead>',
-'thead_close'=> '</thead>',
-'heading_row_start'=> '<tr>',
-'heading_row_end'=>' </tr>',
-'heading_cell_start'=> '<th>',
-'heading_cell_end' => '</th>',
-'tbody_open' => '<tbody>',
-'tbody_close' => '</tbody>',
-'row_start' => '<tr>',
-'row_end'  => '</tr>',
-'cell_start' => '<td>',
-'cell_end' => '</td>',
-'row_alt_start' => '<tr>',
-'row_alt_end' => '</tr>',
-'cell_alt_start' => '<td>',
-'cell_alt_end' => '</td>',
-'table_close' => '</table>'
-);
+<h1 class="display-4 text-center my-4">Roky Závodů</h1>
 
-$table->setTemplate($template);
-echo $table->generate();
-?>
+<div class="mx-3 d-grid gap-2 col-md-6 mx-auto">
+    <?php foreach($raceYear as $row): ?>
+        <a href="<?= site_url('zavody/rok/' . $row->year); ?>" class="btn btn-outline-dark btn-lg d-flex justify-content-between align-items-center">
+            <span>Rok: <strong><?= esc($row->year); ?></strong></span>
+            <span class="badge bg-dark rounded-pill"><?= esc($row->pocet); ?> závodů</span>
+        </a>
+    <?php endforeach; ?>
+</div>
 
 <?=$this->endSection();?>
