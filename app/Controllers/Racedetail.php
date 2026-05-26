bcd142fb2d2ea4e6bcc9c8a77aa87b7c783986b7
Ylamicka <jezekvkybly@gmail.com>
2026-05-26 10:32:06 +0200

    commit #12

--- app/Controllers/Racedetail.php
+++ app/Controllers/Racedetail.php
@@ -4,11 +4,15 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\RaceStages as Rs;
 
 class Racedetail extends BaseController
 {
     public function index()
     {
+        $racestage = new Rs();
+        $dataRaceStage = $racestage->select();
+        
         return view("racedetail");
     }
 }
--- app/Views/racedetail.php
+++ app/Views/racedetail.php
@@ -1,1 +1,5 @@
+<?=$this->extend("layout/master");?>
+
+<?=$this->section("content");?>
 <h1>Ky$ :3</h1>
+<?=$this->endSection();?>
