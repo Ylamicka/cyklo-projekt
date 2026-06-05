cf6865f6130c1ab1ba015036d3daaf5f7f49f0dc
Ylamicka <jezekvkybly@gmail.com>
2026-06-05 11:34:52 +0200

    commit #14

--- app/Views/cyklo.php
+++ app/Views/cyklo.php
@@ -4,7 +4,7 @@
 
 <h1 class="display-4 text-center my-4">Roky Závodů</h1>
 
-<div class="mx-3 d-grid gap-2 col-md-6 mx-auto">
+<div class="d-grid gap-2 col-md-6 mx-auto">
     <?php foreach($raceYear as $row): ?>
         <a href="<?= site_url('zavody/rok/' . $row->year); ?>" class="btn btn-outline-dark btn-lg d-flex justify-content-between align-items-center">
             <span>Rok: <strong><?= esc($row->year); ?></strong></span>
